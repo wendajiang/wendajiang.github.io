8a068b44fecad86e92d0dc514729f1cc9849464e
David <wendajiang93@163.com>
2026-03-07 13:59:24 +0800

    docs: ignore house maintaince

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "wendajiang.github.io",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "content/area/house_fitment"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
